65d332be629ec0f045f9de1da021c0ef9252c1b8
Gabriel Queiroz notebook alana <gabrielanime023@gmail.com>
2025-03-12 11:03:43 -0300

    criação de páginas e rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,38 @@
 import React from "react"
+import { BrowserRouter, Routes, Route, Navigation } from "react-router-dom"
+import login from "./pages/login"
+import register from "./pages/register"
+import home from "./pages/home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigation to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <register />
+}
 
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <route
+        path="/"
+        element={
+         <ProtectedRoute>
+          <home /> 
+         </ProtectedRoute>  
+      }
+        />
+        <Route path="/Login" element={<login/>} /> 
+        <Route path="/Register" element={<RegisterAndLogout/>} /> 
+        <Route path="*" element={<NotFound/>} />        
+      </Routes>  
+    </BrowserRouter>
   )
 }
 
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,17 +1,50 @@
 import { navigate } from 'react-router-dom'
 import { jwtdecode } from 'jwt-decode'
-import api from '../api'
-import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants'
+import api from '../api.js'
+import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants.js'
+import { useState, useEffect } from 'react'
 
 function ProtectedRoute({ children }) {
     const [isAuthorized, setIsAuthorized] = useState(null)
 
-    const RefreshToken = async () => {
+    useEffect(() => {
+        auth().catch(() => setIsAuthorized(false))
+    }, [])
 
+    const RefreshToken = async () => {
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN)
+        try {
+        const res = await api.post("/api/token/refresh/", { 
+            refresh: refreshToken 
+        }); 
+        if (res.status === 200){
+            localStorage.setitem(ACCESS_TOKEN, res.data.access)
+            setIsAuthorized(true)
+        } else {
+            setIsAuthorized(false)
+        }
+    }catch (error) {
+            console.log(error)
+            setIsAuthorized(false)
+        }
     }
 
     const auth = async () => {
+        const token = localStorage.getItem(ACCESS_TOKEN)    
+        if (!token) {
+            setIsAuthorized(false)
+            return
+            
+        }
+        const decoded = jwtdecode(token)
+        const tokenExpiration = decoded.exp * 1000
+        const now = Date.now() / 1000
 
+        if (tokenExpiration < now ) {   
+            await RefreshToken()
+        } else {
+            setIsAuthorized(true)   
+        }
     }
 
     if (isAuthorized === null) {
