cf936617b8e31d6c5a88fb826bc26cfc799c140a
Gabriel Queiroz notebook alana <gabrielanime023@gmail.com>
2025-03-16 23:48:04 -0300

    models criados e dados inseridos

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("notes/", views.NoteListCreateView.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>/",
-         views.NoteDeleteView.as_view(), name="delete-note"),
+    path("notes/delete/<int:pk>/",views.NoteDeleteView.as_view(), name="delete-note"),
 ]
